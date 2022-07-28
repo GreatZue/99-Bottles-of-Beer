95501e316640af49a9dd8e746051d6cd9c779cd4
GreatZue <greatzue@gmail.com>
2022-07-27 21:00:42 -0600

    cleanup

--- script.js
+++ script.js
@@ -6,5 +6,6 @@ while(itemCount > 0) {
     console.log(itemCount + " " + word + " of " + drink + " on the wall!  " + itemCount + " " + word + " of " + drink + "!")
     itemCount--;
     console.log("Take one down, pass it around!")
+    console.log(itemCount + " " + word + " of" + drink + "on the wall!")
 }
 console.log("No more beer!");
